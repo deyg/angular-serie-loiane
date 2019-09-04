c8e73fc6387c81b5e992116b3d099756b15cd133
deyg <denilson.souza@live.com>
2019-09-04 20:20:00 -0300

    vinculando conteudo com interpolacao e property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { MeuPrimeiroComponente } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 import { CursosModule } from './cursos/cursos.module';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponente,
-    MeuPrimeiro2Component
+    MeuPrimeiro2Component,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url = 'http://loiane.com';
+  cursoAngular = true;
+  urlImagem = 'http://lorempixel.com/400/200/nature/';
+
+  interpolation = '\<img src\=\"\{\{ urlImagem \}\}\"\>';
+  propertyBinding = '\<img \[src\] \= \"urlImagem\"\>';
+
+  // function getValor() em TS
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
